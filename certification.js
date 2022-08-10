d6ad5d65e1ea88b5bf71d45c7b753fd5fb30b2d6
mteusbarbosa <mateus.bm13@gmail.com>
2022-08-10 11:08:26 -0300

    Criação do documento

---
+++ certification.js
@@ -0,0 +1,27 @@
+// Aula 01
+
+//This is a comment
+/* This is also a comment */
+
+
+//Aula 02
+let myVar;
+
+//Aula 03
+let a1;
+a1 = 7; 
+
+//Aula 04 - Assigning the Value of One Variable to Another
+let a;
+a = 7;
+let b;
+b = a;
+
+//Aula 05 - Initializing Variables with the Assignment Operator
+let a2 = 9;
+
+//Aula 06 - Declare String Variables
+let myFirstName = 'Mateus';
+let myLastName = 'Barbosa';
+
+//Aula 07 - Understanding Uninitialized Variables
