7a7043a2b931b9bd3b0bd33c95d5d0607129ac81
mteusbarbosa <mateus.bm13@gmail.com>
2022-09-21 21:56:26 -0300

    Atualização das pastas e continuação no módulo JS

--- JavaScript and Data Structures/JAVASCRIPT/certification.js
+++ JavaScript and Data Structures/JAVASCRIPT/certification.js
@@ -135,7 +135,6 @@ const badStr = 'Finn responds, "Let's go!"';
 const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
 
 //Escape Sequences in Strings
-
 /**
     \'	single quote
     \"	double quote
@@ -149,3 +148,119 @@ const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
 const myStr3 = "FirstLine\n\t\\SecondLine\nThirdLine";
 
 //Concatenating Strings with Plus Operator
+const myStr4 = "This is the start. "+"This is the end.";
+
+//Concatenating Strings with the Plus Equals Operator
+let myStr5;
+myStr5 = "This is the first sentence. "
+myStr5 += "This is the second sentence."
+
+//Constructing Strings with Variables
+const myName6 = "Mateus";
+const myStr6 = "My name is " +myName6 + ". I am well!";
+
+//Appending Variables to Strings
+const someAdjective = "awsome";
+let myStr7 = "Learning to code is ";
+myStr7 += someAdjective;
+
+//Find the length of a String
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+
+//Use bracket notation to find the first character in a string
+let firstLetterOfLastName = "";
+const lastName2 = "Lovelace";
+
+firstLetterOfLastName = lastName2[0];
+
+//Understand string immutability
+let myStr8 = "Jello World";
+myStr8 = "Hello World";
+
+//Use bracket notation to find the nth character in a string
+const lastName3 = "Lovelace";
+const thirdLetterOfLastName = lastName3[2]; 
+
+//Use bracket notation to find the last character in a string
+const lastName4 = "Lovelace";
+const lastLetterOfLastName = lastName4[lastName4.length - 1];
+
+//Use bracket notation to find the nth to last character in a string
+const lastName5 = "Lovelace";
+const secondToLastLetterOfLastName = lastName5[lastName5.length - 2];
+
+//Word blanks
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "My "+ myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " !";
+
+//Store multiple values in one variable using javascript arrays
+const myArray = ["String", 1];
+
+//Nest one array within another array
+const myArray2 = [[],[]];
+
+//Access array data with indexes
+const myArray3 = [50, 60, 70];
+let myData = myArray[0];
+
+//Modify array data with indexes
+const myArray4 = [18, 64, 99];
+myArray4[0] = 45
+
+//Access multi dimensional arrays with indexes
+const myArray5 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData5 = myArray5[2][1];
+
+//Manipulate arrays with push
+const myArray6 = [["John", 23], ["cat", 2]];
+
+myArray6.push(["dog", 3]);
+
+//Manipulate arrays with pop
+const myArray7 = [["John", 23], ["cat", 2]];
+let removedFromMyArray;
+
+removedFromMyArray = myArray7.pop();
+
+//Manipulate arrays with shift
+const myArray8 = [["John", 23], ["dog", 3]];
+let removedFromMyArray1;
+
+removedFromMyArray1 = myArray8.shift();
+
+//Manipulate arrays with unshift
+const myArray9 = [["John", 23], ["dog", 3]];
+myArray9.shift();
+
+myArray9.unshift(["Paul", 35]);
+
+//Shopping list
+const myList = [["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15]];
+
+//Write reusable javascript with functions
+function reusableFunction(){
+  console.log("Hi World");
+}
+
+reusableFunction();
+
+//Passing values to functions with arguments
+function functionWithArgs(arg1, arg2){
+  console.log(arg1 + arg2);
+}
+
+functionWithArgs(1, 2);
+
