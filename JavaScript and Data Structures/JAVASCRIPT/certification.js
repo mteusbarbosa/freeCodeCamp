e51139d21b9cea57b84413e4466569ce7fe1d2de
mteusbarbosa <mateus.bm13@gmail.com>
2022-11-01 21:46:40 -0300

    Continuação dos exercícios de JS

--- JavaScript and Data Structures/JAVASCRIPT/certification.js
+++ JavaScript and Data Structures/JAVASCRIPT/certification.js
@@ -8,7 +8,7 @@ let myVar;
 
 //Storing Values with the Assignment Operator
 let a;
-a = 7; 
+a = 7;
 
 //Assigning the Value of One Variable to Another
 let a1;
@@ -47,9 +47,9 @@ let catSound = "Meow!";
 
 //Declare a Read-Only Variable with the const Keyword
 const FCC = "freeCodeCamp";  //Const variables are all upper case
-let fact = "is cool!"; 
+let fact = "is cool!";
 fact = "is awesome!";
-console.log(FCC, fact); 
+console.log(FCC, fact);
 
 //Add Two Numbers with JavaScript
 const sum = 10 + 10;
@@ -148,7 +148,7 @@ const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
 const myStr3 = "FirstLine\n\t\\SecondLine\nThirdLine";
 
 //Concatenating Strings with Plus Operator
-const myStr4 = "This is the start. "+"This is the end.";
+const myStr4 = "This is the start. " + "This is the end.";
 
 //Concatenating Strings with the Plus Equals Operator
 let myStr5;
@@ -157,7 +157,7 @@ myStr5 += "This is the second sentence."
 
 //Constructing Strings with Variables
 const myName6 = "Mateus";
-const myStr6 = "My name is " +myName6 + ". I am well!";
+const myStr6 = "My name is " + myName6 + ". I am well!";
 
 //Appending Variables to Strings
 const someAdjective = "awsome";
@@ -182,7 +182,7 @@ myStr8 = "Hello World";
 
 //Use bracket notation to find the nth character in a string
 const lastName3 = "Lovelace";
-const thirdLetterOfLastName = lastName3[2]; 
+const thirdLetterOfLastName = lastName3[2];
 
 //Use bracket notation to find the last character in a string
 const lastName4 = "Lovelace";
@@ -198,13 +198,13 @@ const myAdjective = "big";
 const myVerb = "ran";
 const myAdverb = "quickly";
 
-const wordBlanks = "My "+ myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " !";
+const wordBlanks = "My " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " !";
 
 //Store multiple values in one variable using javascript arrays
 const myArray = ["String", 1];
 
 //Nest one array within another array
-const myArray2 = [[],[]];
+const myArray2 = [[], []];
 
 //Access array data with indexes
 const myArray3 = [50, 60, 70];
@@ -216,13 +216,13 @@ myArray4[0] = 45
 
 //Access multi dimensional arrays with indexes
 const myArray5 = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [[10, 11, 12], 13, 14],
-  ];
-  
-  const myData5 = myArray5[2][1];
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData5 = myArray5[2][1];
 
 //Manipulate arrays with push
 const myArray6 = [["John", 23], ["cat", 2]];
@@ -248,19 +248,388 @@ myArray9.shift();
 myArray9.unshift(["Paul", 35]);
 
 //Shopping list
-const myList = [["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15],["Chocolate Bar", 15]];
+const myList = [["Chocolate Bar", 15], ["Chocolate Bar", 15], ["Chocolate Bar", 15], ["Chocolate Bar", 15], ["Chocolate Bar", 15], ["Chocolate Bar", 15]];
 
 //Write reusable javascript with functions
-function reusableFunction(){
+function reusableFunction() {
   console.log("Hi World");
 }
 
 reusableFunction();
 
 //Passing values to functions with arguments
-function functionWithArgs(arg1, arg2){
+function functionWithArgs(arg1, arg2) {
   console.log(arg1 + arg2);
 }
 
 functionWithArgs(1, 2);
+
+//Return a value from a function with return
+function timesFive(arg1) {
+  return arg1 * 5;
+}
+
+timesFive(5);
+
+//Global scope and functions
+let myGlobal = 10;
+
+function fun1() {
+  /*
+  Variáveis que são declaradas sem a palavra-chave 
+  let ou const são automaticamente criadas no escopo global
+  */
+  oopsGlobal = 5;
+
+}
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+//Local scope and functions
+function myLocalScope() {
+  let myVar;
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+console.log('outside myLocalScope', myVar);
+
+//Global vs local scope in functions
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  let outerWear = "sweater";
+  return outerWear;
+}
+
+myOutfit();
+
+//Understanding undefined value returned from a function
+let sum1 = 0;
+function addThree() {
+  sum1 = sum1 + 3;
+}
+function addFive() {
+  sum1 = sum1 + 5;
+}
+addThree();
+addFive();
+
+//Assignment with a returned value
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7);
+
+//Stand in line
+function nextInLine(arr, item) {
+  arr.push(item);
+  item = arr.shift();
+  return item;
+}
+
+const testArr = [1, 2, 3, 4, 5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+//Understanding boolean values
+function welcomeToBooleans() {
+  return true;
+}
+
+//Use conditional logic with if statements
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false"
+}
+
+//Comparison with the equality operator
+function testEqual(val) {
+  if (val == 12) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+testEqual(10);
+
+//Comparison with the strict equality operator
+function testStrict(val) {
+  if (val === 7) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+testStrict(10);
+
+//Practice comparing different values
+function compareEquality(a, b) {
+  if (a === b) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+compareEquality(10, "10");
+
+//Comparison with the inequality operator
+function testNotEqual(val) {
+  if (val != 99) { // Altere esta linha
+    return "Not Equal";
+  }
+  return "Equal";
+}
+testNotEqual(10);
+
+//Comparison with the strict inequality operator
+function testStrictNotEqual(val) {
+  if (val !== 17) { // Altere esta linha
+    return "Not Equal";
+  }
+  return "Equal";
+}
+testStrictNotEqual(10);
+
+//Comparison with the greater than operator
+function testGreaterThan(val) {
+  if (val > 100) {
+    return "Over 100";
+  }
+
+  if (val > 10) {
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+testGreaterThan(10);
+
+//Comparison with the greater than or equal to operator
+function testGreaterOrEqual(val) {
+  if (val >= 20) {
+    return "20 or Over";
+  }
+  if (val >= 10) {
+    return "10 or Over";
+  }
+  return "Less than 10";
+}
+testGreaterOrEqual(10);
+
+//Comparison with the less than operator
+function testLessThan(val) {
+  if (val < 25) {  // Altere esta linha
+    return "Under 25";
+  }
+  if (val < 55) {  // Altere esta linha
+    return "Under 55";
+  }
+  return "55 or Over";
+}
+testLessThan(10);
+
+//Comparison with the less than or equal to operator
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Altere esta linha
+    return "Smaller Than or Equal to 12";
+  }
+  if (val <= 24) {  // Altere esta linha
+    return "Smaller Than or Equal to 24";
+  }
+  return "More Than 24";
+}
+testLessOrEqual(10);
+
+//Comparisons with the logical and operator
+function testLogicalAnd(val) {
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+  return "No";
+}
+testLogicalAnd(10);
+
+//Comparisons with the logical or operator
+function testLogicalOr(val) {
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+  return "Inside";
+}
+testLogicalOr(15);
+
+//Introducing else statements
+function testElse(val) {
+  let result = "";
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+  return result;
+}
+
+testElse(4);
+
+//Introducing else if statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+testElseIf(7);
+
+//Logical order in if else statements
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+orderMyLogic(4);
+
+//Chaining If Else Statements
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+}
+
+testSize(7);
+
+//Golf Code
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+if (strokes == 1){
+  return names[0];
+} else if(strokes <= par -2){
+  return names[1];
+} else if(strokes == par -1){
+  return names[2];
+} else if(strokes == par){
+  return names[3];
+} else if(strokes == par + 1){
+  return names[4];
+} else if(strokes == par + 2){
+  return names[5];
+} else {
+  return names[6];
+}
+}
+
+golfScore(5, 4);
+
+//Selecting from Many Options with Switch Statements
+function switchOfStuff(val) {
+  let answer = "";
+  switch (val) {
+  case "a":
+    answer = "apple";
+    break;
+  case "b":
+    answer = "bird";
+    break;
+  case "c":
+    answer = "cat";
+    break;
+  default:
+    answer = "stuff";
+    break;
+  }
+  return answer;
+}
+
+switchOfStuff(1);
+
+//Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+  let answer = "";
+  switch(val){
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5: 
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+
+sequentialSizes(1);
+
+//Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val){
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+//Returning Boolean Values from Functions
+function isLess(a, b) {
+  // Only change code below this line
+  return a <= b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
 
